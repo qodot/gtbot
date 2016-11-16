31b48361b4f56facd09f04c6ebf34db010dfb9f2
qodot <sdaewon12@gmail.com>
2016-11-16 23:43:44 +0900

    Add Google Translator

--- bot.py
+++ bot.py
@@ -1,23 +1,27 @@
 import sys
 import json
 
+import requests
 from slacker import Slacker
 from websocket import create_connection
 
 
 class ChatHandler:
-    def __init__(self, slacker):
+    def __init__(self, slacker, translator):
         self._chat = slacker.chat
         resp = slacker.rtm.start()
         self._socket = create_connection(resp.body['url'])
+        self._translator = translator
 
     def loop(self):
         while True:
-            ch, msg = self.read()
-            if ch and msg:
-                self.send(ch, msg)
+            ch, msg = self._read()
+            if not ch or not msg:
+                continue
+            translated_msg = self._translator.translate(msg)
+            self._send(ch, translated_msg)
 
-    def read(self):
+    def _read(self):
         parsed = None
         while True:
             event = json.loads(self._socket.recv())
@@ -34,18 +38,43 @@ class ChatHandler:
         else:
             return None, None
 
-    def send(self, ch, msg):
+    def _send(self, ch, msg):
         self._chat.post_message(ch, msg, as_user=True)
 
 
-def run(token):
-    slacker = Slacker(token)
-    chat_handler = ChatHandler(slacker)
+class Translator:
+    base_url = 'https://translation.googleapis.com/language/translate/v2'
+
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def translate(self, msg, target='en'):
+        params = {
+            'key': self.api_key,
+            'target': target,
+            'q': msg,
+        }
+        resp = requests.get(self.base_url, params=params).json()
+        return resp['data']['translations'][0]['translatedText']
+
+    def availables(self):
+        params = {
+            'key': self.api_key,
+        }
+        resp = requests.get(self.base_url + '/languages', params=params).json()
+        langs = [lang['language'] for lang in resp['data']['languages']]
+        return ', '.join(langs)
+
+
+def run(slack_token, google_token):
+    slacker = Slacker(slack_token)
+    chat_handler = ChatHandler(slacker, Translator(google_token))
 
     print('Start event loop...')
     chat_handler.loop()
 
 
 if __name__ == '__main__':
-    token = sys.argv[1]
-    run(token)
+    slack_token = sys.argv[1]
+    google_token = sys.argv[2]
+    run(slack_token, google_token)
